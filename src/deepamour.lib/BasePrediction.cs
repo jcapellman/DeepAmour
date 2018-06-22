158e574600ca14928474c5f41571af526a2d4d92
Jarred Capellman <jcapellman@hotmail.com>
2018-06-21 19:23:49 -0500

    Added: Abstraction layer to generalize the interface to load the models and run predictions

--- src/deepamour.lib/BasePrediction.cs
+++ src/deepamour.lib/BasePrediction.cs
@@ -1,6 +1,56 @@
-﻿namespace deepamour.lib
+﻿using System.IO;
+
+using deepamour.lib.MusicPredictor.Objects;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+namespace deepamour.lib
 {
-    public abstract class BasePrediction
+    public abstract class BasePrediction<T, TK> where T : class where TK : class, new()
     {
+        protected abstract string ModelName { get; }
+
+        protected abstract string OutputColumn { get; }
+
+        protected abstract string[] InputColumns { get; }
+
+        private PredictionModel<T, TK> _model;
+
+        private async void LoadDataAsync()
+        {
+            if (File.Exists(ModelName))
+            {
+                _model = await PredictionModel.ReadAsync<T, TK>(ModelName);
+            }
+
+            var pipeline = new LearningPipeline
+            {
+                new TextLoader(ModelName).CreateFrom<MusicData>(separator: ','),
+                new TextFeaturizer(OutputColumn, InputColumns),
+                new FastTreeBinaryClassifier
+                {
+                    NumLeaves = 5,
+                    NumTrees = 5,
+                    MinDocumentsInLeafs = 2
+                }
+            };
+
+            _model = pipeline.Train<T, TK>();
+
+            await _model.WriteAsync(ModelName);
+        }
+
+        public TK Predict(T data)
+        {
+            if (_model == null)
+            {
+                LoadDataAsync();
+            }
+
+            return _model.Predict(data);
+        }
     }
 }
--- src/deepamour.lib/MusicPredictor/MusicPrediction.cs
+++ src/deepamour.lib/MusicPredictor/MusicPrediction.cs
@@ -1,44 +1,13 @@
-﻿using System.IO;
-using System.Threading.Tasks;
-
-using deepamour.lib.MusicPredictor.Objects;
-
-using Microsoft.ML;
-using Microsoft.ML.Data;
-using Microsoft.ML.Trainers;
-using Microsoft.ML.Transforms;
+﻿using deepamour.lib.MusicPredictor.Objects;
 
 namespace deepamour.lib.MusicPredictor
 {
-    public class MusicPrediction
+    public class MusicPrediction : BasePrediction<MusicData, MusicDataPrediction>
     {
-        private const string MODEL_NAME = "music.mdl";
+        protected override string ModelName => "music.mdl";
 
-        private PredictionModel<MusicData, MusicDataPrediction> _model;
+        protected override string OutputColumn => "Features";
 
-        public async Task<bool> LoadDataAsync(string fileName)
-        {
-            if (File.Exists(MODEL_NAME))
-            {
-                _model = await PredictionModel.ReadAsync<MusicData, MusicDataPrediction>(MODEL_NAME);
-                
-                return true;
-            }
-
-            var pipeline = new LearningPipeline
-            {
-                new TextLoader(fileName).CreateFrom<MusicData>(separator: ','),
-                new TextFeaturizer("Features", "SentimentText"),
-                new FastTreeBinaryClassifier {NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2}
-            };
-
-            _model = pipeline.Train<MusicData, MusicDataPrediction>();
-
-            await _model.WriteAsync("music.mdl");
-
-            return true;
-        }
-
-        public MusicDataPrediction Run(MusicData data) => _model.Predict(data);
+        protected override string[] InputColumns => new[] { "SentimentText" };
     }
 }
