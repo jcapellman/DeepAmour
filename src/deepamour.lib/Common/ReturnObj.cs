0fef93b1e3767961843a6d8fa149d923b6a8646e
Jarred Capellman <jcapellman@hotmail.com>
2018-06-24 08:10:30 -0500

    Added: ReturnObj to containize response objects

---
+++ src/deepamour.lib/Common/ReturnObj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace deepamour.lib.Common
+{
+    public class ReturnObj<T>
+    {
+        private readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
+        public T Value { get; private set; }
+
+        public bool IsNullOrError => Value == null || Error != null;
+
+        public Exception Error { get; private set; }
+
+        public ReturnObj(T obj)
+        {
+            Value = obj;
+        }
+
+        public ReturnObj(Exception error)
+        {
+            Value = default(T);
+            Error = error;
+
+            Log.Error(error);
+        }
+    }
+}
