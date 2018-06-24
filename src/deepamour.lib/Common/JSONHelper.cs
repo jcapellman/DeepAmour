325cc785c9d0941319eb563e8dec482950829f27
Jarred Capellman <jcapellman@hotmail.com>
2018-06-24 08:24:10 -0500

    Added: JSON Extension Helper method

---
+++ src/deepamour.lib/Common/JSONHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace deepamour.lib.Common
+{
+    public static class JSONHelper
+    {
+        public static ReturnObj<T> DeserializeFromJson<T>(this string jsonString)
+        {
+            if (string.IsNullOrEmpty(jsonString))
+            {
+                return new ReturnObj<T>(new ArgumentNullException(nameof(jsonString)));
+            }
+
+            try
+            {
+                return new ReturnObj<T>(JsonConvert.DeserializeObject<T>(jsonString));
+            }
+            catch (Exception ex)
+            {
+                return new ReturnObj<T>(ex);
+            }
+        }
+    }
+}
