bf447e182c90af259a83a460ad268fde25f29861
Jarred Capellman <jcapellman@hotmail.com>
2018-06-27 21:18:15 -0500

    Added: Results Page

--- src/deepamour.web/Controllers/HomeController.cs
+++ src/deepamour.web/Controllers/HomeController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+
+using deepamour.lib.core.Predictors.WarriorsPredictor;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace deepamour.web.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index() => View();
+
+        public async Task<ActionResult> RunModel()
+        {
+            var predictor = new WarriorsPredictor();
+
+            var result = await predictor.RunPredictorAsync("");
+
+            return View("Results", result.Value);
+        }
     }
 }
